b950e42cfacfd2b4ed6f996d583699bbda4cb7bc
Rivano553 <rivano.putra@ti.ukdw.ac.id>
2026-06-10 07:27:05 +0700

    commit

---
+++ src/main/java/id/ac/ukdw/rplbo/wacanalibrary/dao/impl/TransaksiDao.java
@@ -0,0 +1,12 @@
+package id.ac.ukdw.rplbo.wacanalibrary.dao;
+
+import id.ac.ukdw.rplbo.wacanalibrary.models.Transaksi;
+import java.util.List;
+
+public interface TransaksiDao {
+    List<Transaksi> getAllTransaksi();
+    void prosesPengembalian(String idTransaksi, String status, String tglKembali, double totalDenda, String metodePembayaran);
+    double getTotalDenda();
+    int countTransaksiByTanggal(String tanggal);
+    java.util.Map<Integer, Integer> getPeminjamanPerBulan(String tahun);
+}
