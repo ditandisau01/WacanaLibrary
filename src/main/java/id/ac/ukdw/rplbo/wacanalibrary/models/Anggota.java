acdbf97cff268a9800721d865162e93342ebeda8
Dita Pranata Tandisau <dita.pranata@ti.ukdw.ac.id>
2026-06-10 07:33:28 +0700

    s

--- src/main/java/id/ac/ukdw/rplbo/wacanalibrary/models/Anggota.java
+++ src/main/java/id/ac/ukdw/rplbo/wacanalibrary/models/Anggota.java
@@ -1,11 +1,11 @@
 package id.ac.ukdw.rplbo.wacanalibrary.models;
+
 import javafx.beans.property.*;
 
 public class Anggota {
     private final StringProperty idAnggota, nim, namaLengkap, tipe, aktifSejak, status;
     private final IntegerProperty batasPinjam;
 
-    // Constructor kini hanya menerima 7 parameter (tanpa username)
     public Anggota(String id, String nim, String nama, String tipe, int batas, String aktifSejak, String status) {
         this.idAnggota = new SimpleStringProperty(id);
         this.nim = new SimpleStringProperty(nim != null ? nim : "-");
@@ -16,11 +16,31 @@ public class Anggota {
         this.status = new SimpleStringProperty(status);
     }
 
-    public StringProperty idAnggotaProperty() { return idAnggota; }
-    public StringProperty nimProperty() { return nim; }
-    public StringProperty namaLengkapProperty() { return namaLengkap; }
-    public StringProperty tipeProperty() { return tipe; }
-    public IntegerProperty batasPinjamProperty() { return batasPinjam; }
-    public StringProperty aktifSejakProperty() { return aktifSejak; }
-    public StringProperty statusProperty() { return status; }
+    public StringProperty idAnggotaProperty() {
+        return idAnggota;
+    }
+
+    public StringProperty nimProperty() {
+        return nim;
+    }
+
+    public StringProperty namaLengkapProperty() {
+        return namaLengkap;
+    }
+
+    public StringProperty tipeProperty() {
+        return tipe;
+    }
+
+    public IntegerProperty batasPinjamProperty() {
+        return batasPinjam;
+    }
+
+    public StringProperty aktifSejakProperty() {
+        return aktifSejak;
+    }
+
+    public StringProperty statusProperty() {
+        return status;
+    }
 }
