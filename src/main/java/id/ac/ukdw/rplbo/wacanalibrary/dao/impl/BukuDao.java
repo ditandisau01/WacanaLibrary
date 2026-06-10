7fa41092d04214daf22be1953c2c6a7744f1fa6c
Rivano553 <rivano.putra@ti.ukdw.ac.id>
2026-06-10 07:26:17 +0700

    commit

---
+++ src/main/java/id/ac/ukdw/rplbo/wacanalibrary/dao/impl/BukuDao.java
@@ -0,0 +1,16 @@
+package id.ac.ukdw.rplbo.wacanalibrary.dao;
+
+import id.ac.ukdw.rplbo.wacanalibrary.models.Buku;
+import java.util.List;
+
+public interface BukuDao {
+    List<Buku> getAllBuku();
+    Buku getBukuById(String idBuku);
+    void tambahBuku(Buku buku);
+    void updateBuku(Buku buku);
+    void hapusBuku(String idBuku);
+    List<Buku> cariBuku(String keyword);
+    int countBukuNonRusak();
+    int countUniqueKategori();
+    java.util.Map<String, Integer> getBukuCountPerKategori();
+}
